efed8b41e784abf49e88b75f5c0c9550e6f78b7b
unknown <soberyl4304@gmail.com>
2025-10-20 14:14:01 +0900

    refactore: split main function to other file

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
 import { Console } from "@woowacourse/mission-utils";
-import ErrorHandler from "./error/ErrorHandler.js";
 
 class App {
   async run() {
-    const input = await Console.readLineAsync(
-      "덧셈할 문자열을 입력해 주세요.\n"
-    );
-
-    ErrorHandler.validateInput(input);
-
-    let numbersString = input;
-    let baseDelimiters = ",:";
-    let customDelimiter = "";
-    console.log("Debug: input", input);
-    const customDelimiterRegex = /\/\/(.*?)\\n/;
-    const match = input.match(customDelimiterRegex);
-
-    console.log("Debug: match", match);
-
-    if (match) {
-      customDelimiter = match[1];
-      numbersString = input.replace(match[0], ",");
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n"
+      );
+      const calculator = new Calculator(input);
+      const result = calculator.calculate();
+      Console.print(`결과 : ${result}`);
+    } catch (error) {
+      Console.print(error.message);
     }
-
-    console.log("Debug: customDelimiter", customDelimiter);
-
-    const escapedDelimiter = customDelimiter.replace(
-      /[-\/\\^$*+?.()|[\]{}]/g,
-      "\\$&"
-    );
-
-    console.log("Debug: numbersString", numbersString);
-    console.log("Debug: escapedDelimiter", escapedDelimiter);
-
-    const finalDelimiters = new RegExp(
-      `[${baseDelimiters}${escapedDelimiter}]`
-    );
-
-    console.log("Debug: finalDelimiters", finalDelimiters);
-
-    const numbers = numbersString.split(finalDelimiters);
-
-    console.log("Debug: numbers", numbers);
-
-    let result = 0;
-    for (const num of numbers) {
-      if (num) {
-        const parsedNum = parseInt(num, 10);
-        ErrorHandler.validateNumber(parsedNum);
-        result += parsedNum;
-      }
-    }
-
-    Console.print(`결과 : ${result}`);
   }
 }
 
---
+++ src/Calculator.js
@@ -0,0 +1,42 @@
+import ErrorHandler from "./error/ErrorHandler.js";
+
+export default class Calculator {
+  constructor(input) {
+    this.input = input;
+  }
+
+  calculate() {
+    let numbersString = this.input;
+    let baseDelimiters = ",:";
+    let customDelimiter = "";
+
+    const customDelimiterRegex = /\/\/(.*?)\n/;
+    const match = this.input.match(customDelimiterRegex);
+
+    if (match) {
+      customDelimiter = match[1];
+      numbersString = this.input.substring(match[0].length);
+    }
+
+    const escapedDelimiter = customDelimiter.replace(
+      /[-\/\\^$*+?.()|[\]{}]/g,
+      "\\$&"
+    );
+    const finalDelimiters = new RegExp(
+      `[${baseDelimiters}${escapedDelimiter}]`
+    );
+
+    const numbers = numbersString.split(finalDelimiters);
+
+    let result = 0;
+    for (const numStr of numbers) {
+      if (numStr) {
+        const parsedNum = parseInt(numStr, 10);
+        ErrorHandler.validateNumber(parsedNum);
+        result += parsedNum;
+      }
+    }
+
+    return result;
+  }
+}
