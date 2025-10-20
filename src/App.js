204bc3e47f13636b158be471e1a23929ebc6f804
unknown <soberyl4304@gmail.com>
2025-10-20 13:33:25 +0900

    feat: add custom delimiter function

--- src/App.js
+++ src/App.js
@@ -4,10 +4,39 @@ class App {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
-    const numbers = input.split(/[,:]/);
+
+    let numbersString = input;
+    let baseDelimiters = ",:";
+    let customDelimiter = "";
+    console.log("Debug: input", input);
+    const customDelimiterRegex = /\/\/(.*?)\\n/;
+    const match = input.match(customDelimiterRegex);
+
+    console.log("Debug: match", match);
+
+    if (match) {
+      customDelimiter = match[1];
+      numbersString = input.replace(match[0], ",");
+    }
+
+    console.log("Debug: customDelimiter", customDelimiter);
+
+    const escapedDelimiter = customDelimiter.replace(
+      /[-\/\\^$*+?.()|[\]{}]/g,
+      "\\$&"
+    );
+
+    const finalDelimiters = new RegExp(
+      `[${baseDelimiters}${escapedDelimiter}]`
+    );
+
+    const numbers = numbersString.split(finalDelimiters);
     let result = 0;
+
     for (const num of numbers) {
-      result += parseInt(num);
+      if (num) {
+        result += parseInt(num, 10);
+      }
     }
 
     Console.print(`결과 : ${result}`);
